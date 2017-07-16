deb92edd424d472205841dbfd8f6e62b7ad59ab9
Your Name <you@example.com>
2017-07-15 21:13:12 -0500

    added concept

--- Duckling.js
+++ Duckling.js
@@ -13,10 +13,10 @@ console.log("[Duckling] DISCORD SET-UP PROPERLY: "+token);
 var wpm = 110;
 //models
 var models = {
-	emotion:{},
-	topics:{},
-	user:{},
-	channel:{}
+    emotion:{},
+    topics:{},
+    user:create(lm.Model)(),
+    channel:{}
 };
 
 //emotions
@@ -46,7 +46,85 @@ duckling.on('ready', () => {
 
 //♥
 
+
+var {EventEmitter} = require("events");
+
+class Tokenizer {
+    constructor() {
+
+    }
+}
+function tokenize (s){
+    return s.replace(/([.!?])/g, "<s>").replace(",","<ph>").split(/\s+/);
+}
+
+class Concept extends EventEmitter {
+    constructor (name,context){
+
+        this.name = name;
+
+        this.models = new Map();
+        this.storage = new Map();
+
+        super();
+
+        this.on("message",this.handleMessage.bind(this));
+    }
+    addModel(name) {
+        if(!this.models.has(name)) {
+            this.models.set(name,create(lm.Model)());
+            this.storage.set(name,create(lm.Storage)());
+        }
+    }
+    classify(string) {
+
+        var gram = new Array(this.context).map(() => null);
+
+        return tokenize(string).reduce((r,t,i) => {
+
+            gram.shift();
+            gram.push(t);
+
+            this.models.forEach((m,k) => {
+                r[k] = (r[k]||1) * m.likelyhood(gram);
+            });
+
+            return r;
+        },{});
+    }
+    static createMap(n,...names) {
+        var o = {};
+        for(name of names) {
+            o[name] = new Concept(name,n);
+        }
+        return o;
+    }
+    train(name,tokens) {
+        if(!this.models.has(name)) {
+            this.addModel(name);
+        }
+        this.models.get(name).train(this.context,tokens);
+    }
+    generateMessage(length) {
+        return this.models.get(name).generateRandomPhrase(length);
+    }
+    handleMessage(k,msg) {
+        this.train(k,tokenize(msg));
+    }
+
+}
+
+const concepts = Concept.createMap("users", "server","channel","duckling");
+
 duckling.on('message', msg => {
+    var user = msg.author.username;
+    var server = msg.guild.name;
+    var channel = msg.channel.name;
+    concepts.users.emit("message",user,msg.content);
+    concepts.server.emit("message",server,msg.content);
+    concepts.channel.emit("message",server,msg.content);
+
+
 
 });
 
